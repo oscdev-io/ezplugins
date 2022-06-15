bab6e7de42564a710439bd2ee170f1b1f727b6f5
Nigel Kukard <nkukard@lbsd.net>
2022-06-15 09:01:54 +0000

    Cleanup docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- Project information -----------------------------------------------------
@@ -68,6 +69,9 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# NK: Add favicon
+html_favicon = "favicon.png"
 
 
 # -- Extension configuration -------------------------------------------------
