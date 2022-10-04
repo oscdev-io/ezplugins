5122460c7f4ad35efc0a825b9141042394f0eb62
Nigel Kukard <nkukard@lbsd.net>
2022-10-04 07:44:53 +0000

    Bumped version to 1.3.4

--- ezplugins/__init__.py
+++ ezplugins/__init__.py
@@ -36,4 +36,4 @@ from .decorators import ezplugin, ezplugin_metadata, ezplugin_method
 from .exceptions import EZPluginError, EZPluginMethodNotFoundError
 from .manager import EZPluginManager
 
-__version__ = "1.3.3"
+__version__ = "1.3.4"
